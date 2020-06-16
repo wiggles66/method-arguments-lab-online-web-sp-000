49ae55a5041304142020431b2bec35efdb238046
Michael Waide <mvwaide@gmail.com>
2020-06-16 17:49:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def introduction(name=Dan)
+def introduction(name="Dan")
 puts "Hi, my name is #{name}."
 end
 
 
-def introduction_with_language(language=Ember.js.)
+def introduction_with_language(language="Ember.js.")
   puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
